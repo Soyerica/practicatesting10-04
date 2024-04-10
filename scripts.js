98d83af5c0e8e165f753d15d49d4e3770badb139
Erica Cleppe <soyerica@gmail.com>
2024-04-10 20:34:10 -0300

    10-04

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function validacion()
+{
+    var  nombre = document.getElementById("nombre").value;
+    var  apellido = document.getElementById("apellido").value;
+    alert(nombre + " " + apellido);
+}
